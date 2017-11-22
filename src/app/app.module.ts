f87e158ea15436d649ef283d5e0a6716b196469c
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2017-11-21 23:22:40 -0500

    Login and signuo basic styles done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { LoginComponent } from "../app/components/login/login.component";
+import { SignupComponent } from "../app/components/signup/signup.component";
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule
